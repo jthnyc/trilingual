ee90e58da7bbee2573711ce57627a1f5c6f8278f
Joanna Huang <joannathhuang@gmail.com>
2020-07-08 11:23:43 +0800

    [Feat]: cart api post route

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const {Product, Order, ProductOrder} = require('../db/models')
+const User = require('../db/models/user')
 module.exports = router
 
 router.get('/:userId', async (req, res, next) => {
@@ -23,6 +24,61 @@ router.get('/:userId', async (req, res, next) => {
     } else {
       res.json([])
     }
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/:userId', async (req, res, next) => {
+  try {
+    const existingOrder = await Order.findOne({
+      where: {
+        userId: req.params.userId,
+        status: 'Pending'
+      }
+    })
+
+    let currentOrder
+
+    if (!existingOrder) {
+      currentOrder = await Order.create()
+      const currentUser = await User.findOne({
+        where: {
+          id: req.params.userId
+        }
+      })
+      currentUser.addOrder(currentOrder)
+    } else {
+      currentOrder = existingOrder
+    }
+
+    let currentProductOrder
+    currentProductOrder = await ProductOrder.findOne({
+      where: {
+        orderId: currentOrder.id,
+        productId: req.body.productId
+      },
+      include: [{model: Product, as: 'product'}]
+    })
+
+    if (!currentProductOrder) {
+      const currentProduct = await Product.findOne({
+        where: {id: req.body.productId},
+        include: [{model: ProductOrder}]
+      })
+      await currentOrder.addProduct(currentProduct)
+      currentProductOrder = await ProductOrder.findOne({
+        where: {
+          orderId: currentOrder.id,
+          productId: req.body.productId
+        },
+        include: [{model: Product, as: 'product'}]
+      })
+    } else {
+      currentProductOrder.quantity++
+      currentProductOrder.save()
+    }
+    res.status(201).removeListener(currentProductOrder)
   } catch (error) {
     next(error)
   }
